ba41eb6f39cc3d11e8be1a904c77d5165b3e4262
Gáspár Nagy <gaspar.nagy@gmail.com>
2024-03-26 18:59:54 +0100

    Undo 'Microsoft.Build.Locator' for MsBuild location and make it flexible with env vars (#75)

--- Reqnroll.TestProjectGenerator/Reqnroll.TestProjectGenerator/MSBuildFinder.cs
+++ Reqnroll.TestProjectGenerator/Reqnroll.TestProjectGenerator/MSBuildFinder.cs
@@ -1,32 +1,64 @@
-using Microsoft.Build.Locator;
 using System;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace Reqnroll.TestProjectGenerator
 {
     public class MSBuildFinder
     {
-        private static readonly VisualStudioInstanceQueryOptions _visualStudioInstanceQueryOptions =
-            new()
-            {
-                DiscoveryTypes =
-                    DiscoveryType.DeveloperConsole
-                    | DiscoveryType.VisualStudioSetup
-                    | DiscoveryType.DotNetSdk
-            };
+        private const string MSBUILD_ENV_VAR = "REQNROLL_TEST_MSBUILD_PATH";
+        private readonly IOutputWriter _outputWriter;
+
+        public MSBuildFinder(IOutputWriter outputWriter)
+        {
+            _outputWriter = outputWriter;
+        }
+
+        /// <summary>
+        /// Source: https://github.com/Microsoft/vswhere/wiki/Installing
+        /// </summary>
+        private string GetVsWherePath()
+        {
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
+            var vsWherePath = Path.Combine(programFiles, "Microsoft Visual Studio", "Installer", "vswhere.exe");
+            return vsWherePath;
+        }
+
+        private string FindUsingVsWhere()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return null;
+
+            var vsWherePath = GetVsWherePath();
+            if (vsWherePath == null || !File.Exists(vsWherePath)) return null;
+
+            // based on instructions at https://github.com/microsoft/vswhere/wiki/Find-MSBuild
+            const string vsWhereMsBuildParameter = @"-latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe";
+            var processResult = new ProcessHelper()
+                .RunProcess(_outputWriter, ".", vsWherePath, vsWhereMsBuildParameter);
+
+            var msBuildPath = 
+                Regex.Split(processResult.CombinedOutput, @"\r?\n")
+                .FirstOrDefault(l => !string.IsNullOrWhiteSpace(l));
+
+            if (msBuildPath == null || !File.Exists(msBuildPath)) return null;
+
+            return msBuildPath;
+        }
+
+        private string FindUsingEnvironmentVariable()
+        {
+            var fromEnv = Environment.GetEnvironmentVariable(MSBUILD_ENV_VAR);
+            if (!string.IsNullOrWhiteSpace(fromEnv)) return fromEnv.Trim();
+            return null;
+        }
 
         public string FindMSBuild()
         {
-            var instance = MSBuildLocator
-                .QueryVisualStudioInstances(_visualStudioInstanceQueryOptions)
-                .FirstOrDefault();
-
-            if (instance == null)
-            {
-                throw new Exception("Unable to locate MSBuild; Please install the dotnet SDK.");
-            }
-
-            return instance.MSBuildPath;
+            return FindUsingVsWhere() ?? 
+                   FindUsingEnvironmentVariable() ??
+                   "msbuild";
         }
     }
 }
