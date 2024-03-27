7083340e4029d47aeb2498f21f0329eabdf200da
Gáspár Nagy <gaspar.nagy@gmail.com>
2024-03-27 12:29:47 +0100

    create Generation test structure

---
+++ Tests/Reqnroll.SystemTests/Generation/GenerationTestBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Reqnroll.SystemTests.Generation;
+
+[TestCategory("Generation")]
+public class GenerationTestBase : SystemTestBase
+{
+    [TestMethod]
+    public void GeneratorAllIn_sample_can_be_handled()
+    {
+        PrepareGeneratorAllInSamples();
+
+        ExecuteTests();
+
+        ShouldAllScenariosPass();
+    }
+}
