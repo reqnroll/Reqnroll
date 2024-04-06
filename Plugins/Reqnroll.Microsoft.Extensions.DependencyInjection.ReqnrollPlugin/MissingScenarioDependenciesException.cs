a75b381f5ed02b8a3c1856dff24c5f7e0a350e2a
Mark Hoek <mbhoek@gmail.com>
2024-04-06 12:00:02 +0200

    Add Microsoft.Extensions.DependencyInjection integration plugin
    Taken from https://github.com/solidtoken/SpecFlow.DependencyInjection/commit/daa540946a7024cae2fbd7027deecc82eca30cb2

---
+++ Plugins/Reqnroll.Microsoft.Extensions.DependencyInjection.ReqnrollPlugin/MissingScenarioDependenciesException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Reqnroll.Microsoft.Extensions.DependencyInjection
+{
+    [Serializable]
+    public class MissingScenarioDependenciesException : ReqnrollException
+    {
+        public MissingScenarioDependenciesException()
+            : base("No method marked with [ScenarioDependencies] attribute found.")
+        {
+            // TODO: Fix help link
+            HelpLink = @"https://github.com/solidtoken/SpecFlow.DependencyInjection#usage";
+        }
+    }
+}
