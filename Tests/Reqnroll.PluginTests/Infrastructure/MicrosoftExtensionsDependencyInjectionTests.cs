9e21d557e622772105f01eaae987843a5327ebab
Mark Hoek <mbhoek@gmail.com>
2024-04-08 21:39:33 +0200

    Add basic MEDI plugin unit test

---
+++ Tests/Reqnroll.PluginTests/Infrastructure/MicrosoftExtensionsDependencyInjectionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Reqnroll.Plugins;
+using Reqnroll.Tracing;
+using Xunit;
+
+namespace Reqnroll.PluginTests.Infrastructure
+{
+    public class MicrosoftExtensionsDependencyInjectionTests
+    {
+        [Fact]
+        public void LoadPlugin_MicrosoftExtensionsDependencyInjection_ShouldNotBeNull()
+        {
+            var loader = new RuntimePluginLoader();
+            var listener = new Mock<ITraceListener>();
+
+            var plugin = loader.LoadPlugin("Reqnroll.Microsoft.Extensions.DependencyInjection.ReqnrollPlugin.dll", listener.Object, It.IsAny<bool>());
+
+            plugin.Should().NotBeNull();
+        }
+    }
+}
