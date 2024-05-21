8c15555c7496aace2e3361cab22d9a1ca2dfeaf3
Gáspár Nagy <gaspar.nagy@gmail.com>
2024-05-21 16:31:03 +0200

    Refactor AutofacPlugin and add unit tests

---
+++ Plugins/Reqnroll.Autofac.ReqnrollPlugin/ConfigurationMethodsProvider.cs
@@ -0,0 +1,15 @@
+﻿using Reqnroll.Bindings;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Reqnroll.Autofac;
+public class ConfigurationMethodsProvider(IBindingRegistry _bindingRegistry) : IConfigurationMethodsProvider
+{
+    public IEnumerable<MethodInfo> GetConfigurationMethods()
+    {
+        return _bindingRegistry.GetBindingAssemblies()
+            .SelectMany(a => a.GetTypes())
+            .SelectMany(t => t.GetMethods(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public));
+    }
+}
