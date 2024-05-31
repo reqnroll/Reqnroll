1b89b3ab0204e32b76a191fa8935eff7d859d359
Gáspár Nagy <gaspar.nagy@gmail.com>
2024-05-31 10:21:12 +0200

    dummy change to trigger build 7

--- Tests/Reqnroll.SystemTests/Portability/PortabilityTestBase.cs
+++ Tests/Reqnroll.SystemTests/Portability/PortabilityTestBase.cs
@@ -151,7 +151,7 @@ public abstract class PortabilityTestBase : SystemTestBase
                 "BeforeTestRun",
                 "AfterTestRun");
             ShouldAllScenariosPass();
-            // dummy change to trigger build 6
+            // dummy change to trigger build 7
             if (unitTestProvider == UnitTestProvider.xUnit && _testRunConfiguration.TargetFramework == TargetFramework.Net462) 
                 throw new Exception("artificial error to show log of successful test");
         });
