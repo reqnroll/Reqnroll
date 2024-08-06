73e48e29d2b68e942ec4b4f083c13b9604f2d415
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-08-06 16:56:02 -0500

    Experiment: Threading.Channel used to pump Messages from TestThreadExecutionEvents to one or more Message Sinks. Default implementation of one Sink that serializes Messages to an ndjson file per feature. Not finished: hooking up the TestThreadExecution event handlers.

---
+++ Reqnroll/CucumberMesssages/CucumberMessageBroker.cs
@@ -0,0 +1,51 @@
+﻿using Reqnroll.BoDi;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Reqnroll.CucumberMesssages
+{
+
+    public interface ICucumberMessageBroker
+    {
+        Task CompleteAsync(string cucumberMessageSource);
+        Task PublishAsync(ReqnrollCucumberMessage message);
+    }
+
+    public class CucumberMessageBroker : ICucumberMessageBroker
+    {
+        private List<ICucumberMessageSink> registeredSinks;
+
+
+        public CucumberMessageBroker(IObjectContainer objectContainer)
+        {
+            var sinks = objectContainer.ResolveAll<ICucumberMessageSink>();
+            registeredSinks = new List<ICucumberMessageSink>(sinks);
+        }
+        public async Task PublishAsync(ReqnrollCucumberMessage message)
+        {
+            foreach (var sink in registeredSinks)
+            {
+                await sink.Publish(message);
+            }
+        }
+
+        // using an empty CucumberMessage to indicate completion
+        public async Task CompleteAsync(string cucumberMessageSource)
+        {
+            var completionMessage = new ReqnrollCucumberMessage
+            {
+                CucumberMessageSource = cucumberMessageSource
+            };
+
+            foreach (var sink in registeredSinks)
+            {
+                await sink.Publish(completionMessage);
+            }
+        }
+    }
+}
