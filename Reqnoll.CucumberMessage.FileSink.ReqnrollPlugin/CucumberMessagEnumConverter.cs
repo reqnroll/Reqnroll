1bb48228227cec44377795e2068234ad009132ea
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-08-06 16:56:02 -0500

    Experiment: Threading.Channel used to pump Messages from TestThreadExecutionEvents to one or more Message Sinks. Default implementation of one Sink that serializes Messages to an ndjson file per feature. Not finished: hooking up the TestThreadExecution event handlers.

---
+++ Reqnoll.CucumberMessage.FileSink.ReqnrollPlugin/CucumberMessagEnumConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Cucumber.Messages
+{
+    internal class CucumberMessageEnumConverter<T> : JsonConverter<T> where T : struct, Enum
+    {
+        private readonly Dictionary<T, string> _enumToString = new();
+        private readonly Dictionary<string, T> _stringToEnum = new();
+
+        protected internal CucumberMessageEnumConverter()
+        {
+            var type = typeof(T);
+            foreach (var field in type.GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                var value = (T)field.GetValue(null)!;
+                var attribute = field.GetCustomAttribute<DescriptionAttribute>();
+                var name = attribute?.Description ?? field.Name;
+                _enumToString[value] = name;
+                _stringToEnum[name] = value;
+            }
+        }
+
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var stringValue = reader.GetString();
+            return _stringToEnum.TryGetValue(stringValue!, out var enumValue) ? enumValue : default;
+        }
+
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(_enumToString.TryGetValue(value, out var stringValue) ? stringValue : value.ToString());
+        }
+    }
+
+}
