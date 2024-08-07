b2265afd78cec37729da4dc4e51fd058c17daff2
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-08-07 17:30:16 -0500

    Exploratory: Extended System Tests to create a CucumberCompatibilityTest project. The File Sink is hooked in as a nuget package. Next: exploratory debugging to confirm the event listeners are wired up correctly at Feature start time.

---
+++ Tests/CucumberMessages.CompatibilityTests/CucumberCompatibilityTestBase.cs
@@ -0,0 +1,18 @@
+﻿using Reqnroll.SystemTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CucumberMessages.CompatibilityTests
+{
+    public class CucumberCompatibilityTestBase : SystemTestBase
+    {
+
+        protected void AddCucumberMessagePlugIn()
+        {
+            _projectsDriver.AddNuGetPackage("Reqnoll.CucumberMessage.FileSink.ReqnrollPlugin", "2.1.0-local");
+        }
+    }
+}
