80af263a8b194ff0e57cadee9762ee5c1d993db5
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-08-09 11:30:06 -0500

    Plugin stucture complete; test case runs, but no output. Need to figure out why trace logging also not working.

--- Reqnroll/CucumberMesssages/CucumberMessagePublisher.cs
+++ Reqnroll/CucumberMesssages/CucumberMessagePublisher.cs
@@ -6,38 +6,55 @@ using System.Text;
 using System.Threading.Tasks;
 using Io.Cucumber.Messages;
 using Io.Cucumber.Messages.Types;
+using Reqnroll.Tracing;
 
 namespace Reqnroll.CucumberMesssages
 {
     public class CucumberMessagePublisher : ICucumberMessagePublisher
     {
         private ICucumberMessageBroker broker;
+        private IObjectContainer objectContainer;
+        private bool initialized = false;
 
-        public CucumberMessagePublisher(ICucumberMessageBroker CucumberMessageBroker)
-        {
+        public CucumberMessagePublisher(ICucumberMessageBroker CucumberMessageBroker, IObjectContainer objectContainer)
+        { 
+            this.objectContainer = objectContainer;
             broker = CucumberMessageBroker;
         }
 
         public void HookIntoTestThreadExecutionEventPublisher(ITestThreadExecutionEventPublisher testThreadEventPublisher)
         {
+            if (initialized)
+            {
+                return;
+            }
+            initialized = true;
+
+            var traceListener = objectContainer.Resolve<ITraceListener>();
+            traceListener.WriteTestOutput("HookIntoTestThreadExecutionEventPublisher");
+
             testThreadEventPublisher.AddHandler<FeatureStartedEvent>(FeatureStartedEventHandler);
             testThreadEventPublisher.AddHandler<FeatureFinishedEvent>(FeatureFinishedEventHandler);
         }
 
         private void FeatureFinishedEventHandler(FeatureFinishedEvent featureFinishedEvent)
         {
-            var featureName = featureFinishedEvent.FeatureContext.FeatureInfo.FolderPath;
-            Task.Run(() => broker.CompleteAsync(featureName));
+            var featureName = featureFinishedEvent.FeatureContext.FeatureInfo.Title;
+            broker.CompleteAsync(featureName);
         }
 
         private void FeatureStartedEventHandler(FeatureStartedEvent featureStartedEvent)
         {
-            var featureName = featureStartedEvent.FeatureContext.FeatureInfo.FolderPath;
-            Task.Run(() => broker.PublishAsync(new ReqnrollCucumberMessage
+            var featureName = featureStartedEvent.FeatureContext.FeatureInfo.Title;
+
+            var traceListener = objectContainer.Resolve<ITraceListener>();
+            traceListener.WriteTestOutput($"FeatureStartedEventHandler: {featureName}");
+
+           broker.PublishAsync(new ReqnrollCucumberMessage
             {
                 CucumberMessageSource = featureName,
                 Envelope = Envelope.Create(new TestCaseStarted(1, "1", "2", "0", new Timestamp(1, 1)))
-            }));
+            });
         }
     }
 }
--- Reqnroll/CucumberMesssages/CucumberMessageSinkBase.cs
+++ Reqnroll/CucumberMesssages/CucumberMessageSinkBase.cs
@@ -1,5 +1,7 @@
-﻿using Reqnroll.Events;
+﻿using Reqnroll.BoDi;
+using Reqnroll.Events;
 using Reqnroll.Plugins;
+using Reqnroll.Tracing;
 using Reqnroll.UnitTestProvider;
 using System.Collections.Generic;
 using System.Threading.Channels;
@@ -8,11 +10,29 @@ using System.Threading.Tasks;
 
 namespace Reqnroll.CucumberMesssages
 {
-    public class CucumberMessageSinkBase : ICucumberMessageSink, IRuntimePlugin
+    public class CucumberMessageSinkBase 
     {
+        protected IObjectContainer _testThreadContainer;
+
+        private ICucumberMessagePublisher _publisher;
         private Channel<ReqnrollCucumberMessage> _channel = Channel.CreateUnbounded<ReqnrollCucumberMessage>();
+
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            runtimePluginEvents.CustomizeTestThreadDependencies += (sender, args) =>
+            {
+                _testThreadContainer = args.ObjectContainer;
+                _publisher = args.ObjectContainer.Resolve<ICucumberMessagePublisher>();
+                var testThreadExecutionEventPublisher = args.ObjectContainer.Resolve<ITestThreadExecutionEventPublisher>();
+                _publisher.HookIntoTestThreadExecutionEventPublisher(testThreadExecutionEventPublisher);
+            };
+        }
+
         public async Task Publish(ReqnrollCucumberMessage message)
         {
+            var traceListener = _testThreadContainer.Resolve<ITraceListener>();
+            traceListener.WriteTestOutput($"Cucumber Message Sink publishing message.");
+
             await _channel.Writer.WriteAsync(message);
             if (message.Envelope == null)
             {
@@ -22,22 +42,14 @@ namespace Reqnroll.CucumberMesssages
 
         public async IAsyncEnumerable<ReqnrollCucumberMessage> Consume()
         {
+            var traceListener = _testThreadContainer.Resolve<ITraceListener>();
+            traceListener.WriteTestOutput($"Cucumber Message Sink Consume() called.");
+
             await foreach (var message in _channel.Reader.ReadAllAsync())
             {
+                //        _traceListener.WriteTestOutput($"Cucumber Message Sink consuming message.");
                 yield return message;
             }
         }
-
-        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
-        {
-            runtimePluginEvents.RegisterGlobalDependencies += (sender, args) => args.ObjectContainer.RegisterInstanceAs<ICucumberMessageSink>(this);
-            runtimePluginEvents.CustomizeTestThreadDependencies += (sender, args) =>
-                {
-                    var publisher = args.ObjectContainer.Resolve<ICucumberMessagePublisher>();
-                    var testThreadExecutionEventPublisher = args.ObjectContainer.Resolve<ITestThreadExecutionEventPublisher>();
-                    publisher.HookIntoTestThreadExecutionEventPublisher(testThreadExecutionEventPublisher);
-                };
-        }
-
     }
 }
