751c315f3c121af8e76f1494b5f2c55e37fb1f53
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-08-15 11:51:53 -0500

    When Pickles can't be created (such as with ExternalData), suspend support for CucumberMessages for that feature.

---
+++ Reqnroll/CucumberMesssages/FeatureState.cs
@@ -0,0 +1,8 @@
+﻿namespace Reqnroll.CucumberMesssages
+{
+    internal class FeatureState
+    {
+        public string Name { get; set; }
+        public bool Enabled { get; set; } //This will be false if the feature could not be pickled
+    }
+}
