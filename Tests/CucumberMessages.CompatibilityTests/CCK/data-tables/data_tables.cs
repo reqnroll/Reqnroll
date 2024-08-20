24fdc102fefd960c6771ab7d69912ca11bd0bff4
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-08-20 16:38:13 -0500

    All CCK examples loaded, except for markdown, retry, and stack-traces which I don't think apply to the .NET implementation. Need to add support for images and documents as embedded resources to be added to generated test projects before attachments and hooks can be implemented and tested. Not yet added infrastructure to compare output ndjon with expected.

---
+++ Tests/CucumberMessages.CompatibilityTests/CCK/data-tables/data_tables.cs
@@ -0,0 +1,110 @@
+﻿using Reqnroll;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CucumberMessages.CompatibilityTests.CCK.data_tables
+{
+    [Binding]
+    internal class data_tables
+    {
+        private Table? _transposedTable;
+
+        [When("the following table is transposed:")]
+        public void WhenTheFollowingTableIsTransposed(Table table)
+        {
+            _transposedTable = Transpose(table);
+        }
+
+        [Then("it should be:")]
+        public void ThenItShouldBe(Table expected)
+        {
+            TablesEqual(expected, _transposedTable!);
+        }
+
+        private void TablesEqual(Table expected, Table transposedTable)
+        {
+            var ExpectednumRows = expected.Rows.Count;
+            var ExpectednumCols = expected.Rows[0].Count;
+
+            if (ExpectednumRows != transposedTable.Rows.Count || ExpectednumCols != transposedTable.Rows[0].Count)
+            {
+                throw new Exception("Tables are not equal");
+            }
+
+            for (int i = 0; i < ExpectednumRows; i++)
+            {
+                for (int j = 0; j < ExpectednumCols; j++)
+                {
+                    if (expected.Rows[i][j].ToString() != transposedTable.Rows[i][j].ToString())
+                    {
+                        throw new Exception("Tables are not equal");
+                    }
+                }
+            }
+        }
+
+        private Table Transpose(Table table)
+        {
+
+            string[][] matrix = GetStringArray(table.Rows);
+            var t = TransposeMatrix(matrix);
+            return CreateTable(t);
+
+            static string[][] GetStringArray(DataTableRows rows)
+            {
+                int numRows = rows.Count;
+                int numCols = rows.FirstOrDefault()?.Count ?? 0;
+
+                string[][] result = new string[numRows][];
+                for (int i = 0; i < numRows; i++)
+                {
+                    result[i] = new string[numCols];
+                    for (int j = 0; j < numCols; j++)
+                    {
+                        result[i][j] = rows[i][j].ToString();
+                    }
+                }
+
+                return result;
+            }
+            static string[][] TransposeMatrix(string[][] matrix)
+            {
+                int numRows = matrix.Length;
+                int numCols = matrix[0].Length;
+
+                string[][] transposedMatrix = new string[numCols][];
+                for (int i = 0; i < numCols; i++)
+                {
+                    transposedMatrix[i] = new string[numRows];
+                }
+
+                for (int i = 0; i < numRows; i++)
+                {
+                    for (int j = 0; j < numCols; j++)
+                    {
+                        transposedMatrix[j][i] = matrix[i][j];
+                    }
+                }
+
+                return transposedMatrix;
+            }
+            static Table CreateTable(string[][] matrix)
+            {
+                var columnCount = matrix[0].Length;
+                var headers = Enumerable.Range(0, columnCount).Select(i => $"").ToArray();
+                var table = new Table();
+
+                foreach (var row in matrix)
+                {
+                    table.AddRow(row);
+                }
+
+                return table;
+            }
+        }
+    }
+}
