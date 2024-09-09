31b2513653dfa3b8bb6f77eeab692b626ad496c9
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-09-09 17:16:30 -0500

    Minimal and Hooks now passing Content and GD checks. Temp patch to Cucumber.Messages - with local build. Will need to revert to global nuget package when PR accepted by them.

--- Tests/CucumberMessages.CompatibilityTests/FluentAsssertionCucumberMessagePropertySelectionRule.cs
+++ Tests/CucumberMessages.CompatibilityTests/FluentAsssertionCucumberMessagePropertySelectionRule.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Cucumber.Messages;
 using FluentAssertions;
 using FluentAssertions.Equivalency;
 
