e33fd77a02ad893f371937c06ab70823d48fab57
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-09-16 14:18:08 -0500

    Modified CucumberMessagesConverter to include transformation of the AST to patch in missing Location elements before the AST is transformed to the Cucumber GherkinDocument. This duplicates the GherkinDocumentVisitor and ScenarioTransformation classes from the External Plugin (pending further refactoring).

---
+++ Reqnroll.Parser/PatchMissingLocationElementsTransformation.cs
@@ -0,0 +1,51 @@
+﻿using Gherkin.Ast;
+using Reqnroll.ExternalData.ReqnrollPlugin.Transformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reqnroll.Parser
+{
+    internal class PatchMissingLocationElementsTransformation : ScenarioTransformation
+    {
+        protected override Scenario GetTransformedScenario(Scenario scenario)
+        {
+            return null;
+        }
+
+        protected override Scenario GetTransformedScenarioOutline(ScenarioOutline scenarioOutline)
+        {
+            if (scenarioOutline.Examples == null || !scenarioOutline.Examples.Any())
+                return null;
+
+            var exampleTables = scenarioOutline.Examples;
+            List<Examples> transformedExamples = new List<Examples>();
+
+            transformedExamples.AddRange(exampleTables.Select(e => PatchLocations(e)));
+            return new ScenarioOutline(
+                scenarioOutline.Tags.ToArray(),
+                PatchLocation(scenarioOutline.Location),
+                scenarioOutline.Keyword,
+                scenarioOutline.Name,
+                scenarioOutline.Description,
+                scenarioOutline.Steps.ToArray(),
+                transformedExamples.ToArray());
+        }
+
+        private Examples PatchLocations(Examples e)
+        {
+            var headerCells = e.TableHeader.Cells;
+            var tableHeader = new Gherkin.Ast.TableRow(PatchLocation(e.TableHeader.Location), headerCells.Select(hc => new Gherkin.Ast.TableCell(PatchLocation(hc.Location), hc.Value)).ToArray());
+            var rows = e.TableBody.Select(r => new Gherkin.Ast.TableRow(PatchLocation(r.Location), r.Cells.Select(c => new Gherkin.Ast.TableCell(PatchLocation(c.Location), c.Value)).ToArray())).ToArray();
+            return new Examples(e.Tags.ToArray(), PatchLocation(e.Location), e.Keyword, e.Name, e.Description, tableHeader, rows);
+        }
+
+        private static Location PatchLocation(Location l)
+        {
+            return l ?? new Location(0, 0);
+        }
+
+
+    }
+}
