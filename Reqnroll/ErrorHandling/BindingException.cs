d23c0764603dcebba5b28619951dcc81949217b6
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2024-09-17 15:23:23 -0500

    Enhanced BindingException with sub-class AmbiguousBindingException to carry along the candidate BindingMatches that cause the ambiguity. These are used in the Step tracker and message factory to populate the array of StepDef Ids that cause the ambiguity.

--- Reqnroll/ErrorHandling/BindingException.cs
+++ Reqnroll/ErrorHandling/BindingException.cs
@@ -1,4 +1,7 @@
+using Reqnroll.Bindings;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 // the exceptions are part of the public API, keep them in Reqnroll namespace
@@ -23,6 +26,52 @@ namespace Reqnroll
             SerializationInfo info,
             StreamingContext context) : base(info, context)
         {
+        }
+    }
+
+    [Serializable]
+    public class AmbiguousBindingException : BindingException
+    {
+        public IEnumerable<BindingMatch> Matches { get; private set; }
+
+        public AmbiguousBindingException()
+        {
+        }
+
+        public AmbiguousBindingException(string message) : base(message)
+        {
+        }
+
+        public AmbiguousBindingException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        public AmbiguousBindingException(string message, IEnumerable<BindingMatch> matches) : base(message)
+        {
+            Matches = new List<BindingMatch>(matches);
+        }
+
+        protected AmbiguousBindingException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException(nameof(info));
+            }
+
+            Matches = (List<BindingMatch>)info.GetValue("Matches", typeof(List<BindingMatch>));
+        }
+
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException(nameof(info));
+            }
+
+            base.GetObjectData(info, context);
+            info.AddValue("Matches", Matches);
         }
     }
 }
