e3421058fe4b4e2db4219a5c404808df642e98ce
Stef Heyenrath <stef.heyenrath@gmail.com>
2024-11-08 13:54:49 +0100

    Add Reqnroll.Microsoft.Extensions.Logging.ReqnrollPlugin

---
+++ Plugins/Reqnroll.Microsoft.Extensions.Logging.ReqnrollPlugin/ReqnrollLoggerOptions.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Reqnroll.Microsoft.Extensions.Logging;
+
+public sealed class ReqnrollLoggerOptions
+{
+    /// <summary>
+    /// Includes scopes when <see langword="true" />.
+    /// </summary>
+    public bool IncludeScopes { get; set; }
+
+    /// <summary>
+    /// Includes category when <see langword="true" />.
+    /// </summary>
+    public bool IncludeCategory { get; set; }
+
+    /// <summary>
+    /// Includes log level when <see langword="true" />.
+    /// </summary>
+    public bool IncludeLogLevel { get; set; }
+
+    /// <summary>
+    /// Gets or sets format string used to format timestamp in logging messages. Defaults to <see langword="null" />.
+    /// </summary>
+    [StringSyntax(StringSyntaxAttribute.DateTimeFormat)]
+    public string? TimestampFormat { get; set; }
+
+    /// <summary>
+    /// Gets or sets indication whether or not UTC timezone should be used to format timestamps in logging messages. Defaults to <see langword="false" />.
+    /// </summary>
+    public bool UseUtcTimestamp { get; set; }
+}
---
+++ Plugins/Reqnroll.Microsoft.Extensions.Logging.ReqnrollPlugin/ReqnrollLoggerProvider.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Logging;
+
+namespace Reqnroll.Microsoft.Extensions.Logging;
+
+public sealed class ReqnrollLoggerProvider : ILoggerProvider
+{
+    private readonly IReqnrollOutputHelper _outputHelper;
+    private readonly ReqnrollLoggerOptions _options;
+    private readonly LoggerExternalScopeProvider _scopeProvider = new();
+
+    public ReqnrollLoggerProvider(IReqnrollOutputHelper outputHelper)
+        : this(outputHelper, options: null)
+    {
+    }
+
+    public ReqnrollLoggerProvider(IReqnrollOutputHelper outputHelper, bool appendScope)
+        : this(outputHelper, new ReqnrollLoggerOptions { IncludeScopes = appendScope })
+    {
+    }
+
+    public ReqnrollLoggerProvider(IReqnrollOutputHelper outputHelper, ReqnrollLoggerOptions? options)
+    {
+        _outputHelper = outputHelper;
+        _options = options ?? new ReqnrollLoggerOptions();
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new ReqnrollLogger(_outputHelper, _scopeProvider, categoryName, _options);
+    }
+
+    public void Dispose()
+    {
+    }
+}
