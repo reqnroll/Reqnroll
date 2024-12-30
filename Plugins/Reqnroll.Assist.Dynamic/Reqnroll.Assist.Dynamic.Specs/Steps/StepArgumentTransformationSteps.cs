71cd58bd2753419d71bc5e127c1198d5fcc94c5d
Oleg Koshmeliuk <oleg.koshmeliuk@onseo.biz>
2024-12-30 23:23:27 +0200

    Add Reqnroll.Assist.Dynamic

---
+++ Plugins/Reqnroll.Assist.Dynamic/Reqnroll.Assist.Dynamic.Specs/Steps/StepArgumentTransformationSteps.cs
@@ -0,0 +1,37 @@
+﻿using Reqnroll.Assist.Dynamic;
+
+namespace AssistDynamic.Specs.Steps
+{
+  [Binding]
+  public class StepArgumentTransformationSteps
+  {
+    private readonly State state;
+
+    public StepArgumentTransformationSteps(State state) => this.state = state;
+
+    [Given(@"I create a set of dynamic instances from this table using step argument transformation")]
+    public void a(IList<dynamic> dynamicSet)
+    {
+      this.state.OriginalSet = dynamicSet;
+    }
+
+    [When(@"I compare the set to this table using step argument transformation")]
+    public void b(Table table)
+    {
+      table.CompareToDynamicSet(this.state.OriginalSet);
+    }
+
+    [Given(@"I create a dynamic instance from this table using step argument transformation")]
+    public void c(dynamic instance)
+    {
+      this.state.OriginalInstance = instance;
+    }
+
+    [When(@"I compare it to this table using step argument transformation")]
+    public void d(Table table)
+    {
+      var org = (object)this.state.OriginalInstance;
+      table.CompareToDynamicInstance(org);
+    }
+  }
+}
---
+++ Plugins/Reqnroll.Assist.Dynamic/Reqnroll.Assist.Dynamic/DynamicSetComparisonException.cs
@@ -0,0 +1,13 @@
+﻿namespace Reqnroll.Assist.Dynamic;
+
+public class DynamicSetComparisonException : Exception
+{
+    public IList<string> Differences { get; private set; }
+
+    public DynamicSetComparisonException(string message) : base(message) { }
+
+    public DynamicSetComparisonException(string message, IList<string> differences) : base(message)
+    {
+        Differences = differences;
+    }
+}
