575a358717d7424a1b777867dfe47b8bd3700734
Oleg Koshmeliuk <oleg.koshmeliuk@onseo.biz>
2025-02-03 15:48:26 +0200

    Add Reqnroll.Assist.Dynamic (#377)
    * Add Reqnroll.Assist.Dynamic
    
    * added to main project
    
    * updated docs WIP
    
    * updated nuspec file

---
+++ Plugins/Reqnroll.Assist.Dynamic/Reqnroll.Assist.Dynamic.Specs/ComparingDynamicInstances.feature
@@ -0,0 +1,61 @@
+﻿Feature: Comparing dynamic instances
+	In order to be able to easy do assertions
+	As a SpecFlow developer
+	I want to be able to compare dynamic instances
+
+Scenario: Matching a dynamic instance against a table
+	Given I create a dynamic instance from this table
+		| Name   | Age | Birth date | Length in meters |
+		| Marcus | 39  | 1972-10-09 | 1.96             |
+	When I compare it to this table
+		| Name   | Age | Birth date | Length in meters |
+		| Marcus | 39  | 1972-10-09 | 1.96             |
+	Then no instance comparison exception should have been thrown
+
+Scenario: Not matching when 1 header differ
+	Given I create a dynamic instance from this table
+		| Name   | 
+		| Marcus | 
+	When I compare it to this table	 
+	   | N      |
+	   | Marcus |
+	Then an instance comparison exception should be thrown with 2 differences
+		And one difference should be on the 'Name' field of the instance
+		And one difference should be on the 'N' column of the table
+
+Scenario: Not matching when 2 header differ
+	Given I create a dynamic instance from this table
+		| Name   | Birth date |
+		| Marcus | 2000-01-01 |
+	When I compare it to this table	 
+	   | N      | Date of birth |
+	   | Marcus | 2000-01-01    |
+	Then an instance comparison exception should be thrown with 4 differences
+		And one difference should be on the 'Name' field of the instance
+		And one difference should be on the 'BirthDate' field of the instance
+		And one difference should be on the 'N' column of the table
+		And one difference should be on the 'DateOfBirth' column of the table
+
+Scenario: Not matching when 1 value differ
+	Given I create a dynamic instance from this table
+		| Name   |
+		| Marcus |
+	When I compare it to this table	 
+	   | Name   |
+	   | Albert |
+	Then an instance comparison exception should be thrown with 1 difference
+		And one difference should be on the 'Name' property
+		And one message should state that the instance had the value 'Marcus'
+		And one message should state that the table had the value 'Albert'
+
+Scenario: Not matching when several value differ
+	Given I create a dynamic instance from this table
+		| Name   | BirthDate  | LengthInMeters |
+		| Marcus | 1972-10-09 | 1.96           |
+	When I compare it to this table	 
+	   | Name   | Birth date | Length in meters |
+	   | Albert | 2008-01-24 | 1.04             |
+	Then an instance comparison exception should be thrown with 3 difference
+		And one difference should be on the 'Name' property
+		And one difference should be on the 'BirthDate' property
+		And one difference should be on the 'LengthInMeters' property
---
+++ Plugins/Reqnroll.Assist.Dynamic/Reqnroll.Assist.Dynamic.Specs/Steps/DynamicInstanceCreationSteps.cs
@@ -0,0 +1,165 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Reqnroll.Assist.Dynamic;
+
+namespace AssistDynamic.Specs.Steps
+{
+  [Binding]
+  public class DynamicInstanceCreationSteps
+  {
+    private readonly State state;
+
+    public DynamicInstanceCreationSteps(State state) => this.state = state;
+
+    [Given(@"I create a dynamic instance from this table")]
+    [When(@"I create a dynamic instance from this table")]
+    public void CreateDynamicInstanceFromTable(Table table)
+    {
+      this.state.OriginalInstance = table.CreateDynamicInstance();
+    }
+
+    [Then(@"the Name property should equal '(.*)'")]
+    public void NameShouldBe(string expectedValue)
+    {
+      var actual = ((string)this.state.OriginalInstance.Name);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+
+    [Then(@"the Age property should equal (\d+)")]
+    public void AgeShouldBe(int expectedAge)
+    {
+      var actualAge = ((int)this.state.OriginalInstance.Age);
+      ClassicAssert.AreEqual(expectedAge, actualAge);
+    }
+
+    [Then(@"the age property should equal (\d+)")]
+    public void LowerCaseAgeShouldBe(int expectedAge)
+    {
+      var actualAge = ((int)this.state.OriginalInstance.age);
+      ClassicAssert.AreEqual(expectedAge, actualAge);
+    }
+
+    [Then(@"the BirthDate property should equal (.*)")]
+    public void BirthDateShouldBe(string expectedDate)
+    {
+      var expected = DateTime.Parse(expectedDate);
+      var actual = ((DateTime)this.state.OriginalInstance.BirthDate);
+      ClassicAssert.AreEqual(expected, actual);
+    }
+
+    [Then(@"the LengthInMeters property should equal '(\d+\.\d+)'")]
+    public void LengthInMeterShouldBe(double expectedLengthInMeters)
+    {
+      CheckLengthInMeters(expectedLengthInMeters);
+    }
+
+    [Then(@"the MolecularWeight property should equal '(\d+\.\d+)'")]
+    public void MolecularWeightShouldBe(decimal expectedMolecularWeight)
+    {
+      CheckMolecularWeight(expectedMolecularWeight);
+    }
+
+    private void CheckLengthInMeters(double expectedLengthInMeters)
+    {
+      var actual = ((double)this.state.OriginalInstance.LengthInMeters);
+      ClassicAssert.AreEqual(expectedLengthInMeters, actual);
+    }
+
+    private void CheckMolecularWeight(decimal expectedMolecularWeight)
+    {
+      var actual = ((decimal)this.state.OriginalInstance.MolecularWeight);
+      ClassicAssert.AreEqual(expectedMolecularWeight, actual);
+    }
+
+    [Then(@"the SATScore should be (\d+)")]
+    public void SATTest(int expectedScore)
+    {
+      var actual = ((int)this.state.OriginalInstance.SATScore);
+      ClassicAssert.AreEqual(expectedScore, actual);
+    }
+
+    [Then(@"the IsDeveloper property should equal '(.*)'")]
+    public void ThenTheIsDeveloperPropertyShouldEqualTrueAndBeOfTypeBool(bool expectedValue)
+    {
+      var actual = ((bool)this.state.OriginalInstance.IsDeveloper);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+
+    [Then(@"the CharpNmeWithStrangeChars property should equal '(.*)'")]
+    public void ThenTheCharpNmeWithStrangeCharsPropertyShouldEqual(string expectedValue)
+    {
+      var actual = ((string)this.state.OriginalInstance.CharpNmeWithStrangeChars);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+
+    [Then(@"the My_Nice_Variable property should equal '(.*)'")]
+    public void ThenTheMy_Nice_VariablePropertyShouldEqual(string expectedValue)
+    {
+      var actual = ((string)this.state.OriginalInstance.My_Nice_Variable);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+
+    [Then(@"the MyVariableNeedsCleanUp property should equal '(.*)'")]
+    public void ThenTheMyVariableNeedsCleanUpPropertyShouldEqual(string expectedValue)
+    {
+      var actual = ((string)this.state.OriginalInstance.MyVariableNeedsCleanUp);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+
+    [When(@"I create a dynamic instance with only reserved chars")]
+    public void OnlyReservedChars(Table table)
+    {
+      try
+      {
+        this.state.OriginalInstance = table.CreateDynamicInstance();
+      }
+      catch (DynamicInstanceFromTableException ex)
+      {
+        state.CurrentException = ex;
+      }
+    }
+
+    [Then(@"an exception with a nice error message about the property only containing reserved chars should be thrown")]
+    public void ThenAnExceptionWithANiceErrorMessageAboutThePropertyOnlyContainingReservedCharsShouldBeThrown()
+    {
+      var ex = state.CurrentException as DynamicInstanceFromTableException;
+      ClassicAssert.NotNull(ex);
+      ClassicAssert.IsTrue(ex.Message.Contains("only contains"));
+    }
+
+    [Given(@"I create a dynamic instance from this table using no type conversion")]
+    [When(@"I create a dynamic instance from this table using no type conversion")]
+    public void WhenICreateADynamicInstanceFromThisTableUsingNoTypeConversion(Table table)
+    {
+      this.state.OriginalInstance = table.CreateDynamicInstance(false);
+    }
+
+    [Then(@"the Name value should still be '(.*)'")]
+    public void ThenTheNameValueShouldStillBe(string expectedValue)
+    {
+      var actual = ((string)this.state.OriginalInstance.Name);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+
+    [Then(@"the Age value should still be '(.*)'")]
+    public void ThenTheAgeValueShouldStillBe(string expectedValue)
+    {
+      var actual = ((string)this.state.OriginalInstance.Age);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+
+    [Then(@"the birth date should still be '(.*)'")]
+    public void ThenTheBirthDateShouldStillBe(string expectedValue)
+    {
+      var actual = ((string)this.state.OriginalInstance.BirthDate);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+
+    [Then(@"length in meter should still be '(.*)'")]
+    public void ThenLengthInMeterShouldStillBe(string expectedValue)
+    {
+      var actual = ((string)this.state.OriginalInstance.LengthInMeters);
+      ClassicAssert.AreEqual(expectedValue, actual);
+    }
+  }
+}
