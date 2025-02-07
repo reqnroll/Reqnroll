d744f30728244011a1ae0f7d044c3c7ce50624d9
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-02-07 15:23:00 +0100

    Fix: Reqnroll.Autofac: FeatureContext cannot be resolved in BeforeFeature/AfterFeature hooks (#437)
    * Fix: Reqnroll.Autofac: FeatureContext cannot be resolved in BeforeFeature/AfterFeature hooks (#340)
    
    * re-add commentted test lines

--- Plugins/Reqnroll.Autofac.ReqnrollPlugin/AutofacTestObjectResolver.cs
+++ Plugins/Reqnroll.Autofac.ReqnrollPlugin/AutofacTestObjectResolver.cs
@@ -9,25 +9,29 @@ namespace Reqnroll.Autofac
     {
         public object ResolveBindingInstance(Type bindingType, IObjectContainer container)
         {
-            if (container.IsRegistered<IComponentContext>())
+            var componentContext = container switch
             {
-                var componentContext = container.Resolve<IComponentContext>();
-                return componentContext.Resolve(bindingType);
+                _ when container.IsRegistered<IComponentContext>() => container.Resolve<IComponentContext>(),
+                _ when container.IsRegistered<ILifetimeScope>() => container.Resolve<ILifetimeScope>(),
+                _ when container.IsRegistered<IContainer>() => container.Resolve<IContainer>(),
+                _ => null
+            };
+
+            if (componentContext == null)
+                return container.Resolve(bindingType);
+
+            if (container.IsRegistered(bindingType))
+            {
+                // If the requested object is registered in the Reqnroll container, 
+                // we give a chance to the Autofac container to resolve
+                // but fall back to the Reqnroll one.
+                if (componentContext.TryResolve(bindingType, out object instance))
+                    return instance;
+                return container.Resolve(bindingType);
             }
 
-            if (container.IsRegistered<ILifetimeScope>())
-            {
-                var lifeTimeScope = container.Resolve<ILifetimeScope>();
-                return lifeTimeScope.Resolve(bindingType);
-            }
-
-            if (container.IsRegistered<IContainer>())
-            {
-                var lifeTimeScope = container.Resolve<IContainer>();
-                return lifeTimeScope.Resolve(bindingType);
-            }
-
-            return container.Resolve(bindingType);
+            // If this is not in Reqnroll container we get it from Autofac and let it fail if it's not there.
+            return componentContext.Resolve(bindingType);
         }
     }
 }
