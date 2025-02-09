b12f8f8dc539b128c87823bb27cfc54c06a93dc9
Adam Pluciński <adaskothebeast@gmail.com>
2025-02-09 13:27:21 +0100

    TUnit test framework

---
+++ Plugins/Reqnroll.TUnit.ReqnrollPlugin/RuntimePlugin.cs
@@ -0,0 +1,26 @@
+﻿using Reqnroll.Infrastructure;
+using Reqnroll.Plugins;
+using Reqnroll.Tracing;
+using Reqnroll.UnitTestProvider;
+
+namespace Reqnroll.TUnit.ReqnrollPlugin;
+
+public class RuntimePlugin : IRuntimePlugin
+{
+    public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+    {
+        runtimePluginEvents.RegisterGlobalDependencies += RuntimePluginEvents_RegisterGlobalDependencies;
+        runtimePluginEvents.CustomizeTestThreadDependencies += RuntimePluginEvents_CustomizeTestThreadDependencies;
+        unitTestProviderConfiguration.UseUnitTestProvider("tunit");
+    }
+    private void RuntimePluginEvents_RegisterGlobalDependencies(object sender, RegisterGlobalDependenciesEventArgs e)
+    {
+        e.ObjectContainer.RegisterTypeAs<TUnitRuntimeProvider, IUnitTestRuntimeProvider>("tunit");
+    }
+    private void RuntimePluginEvents_CustomizeTestThreadDependencies(object sender, CustomizeTestThreadDependenciesEventArgs e)
+    {
+        var container = e.ObjectContainer;
+        container.RegisterTypeAs<TUnitTraceListener, ITraceListener>();
+        container.RegisterTypeAs<TUnitAttachmentHandler, IReqnrollAttachmentHandler>();
+    }
+}
