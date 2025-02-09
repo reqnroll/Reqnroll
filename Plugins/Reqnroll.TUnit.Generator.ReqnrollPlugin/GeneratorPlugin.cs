64f14e4082eb32bf72cacdfb0e307bba239f4e71
Adam Pluciński <adaskothebeast@gmail.com>
2025-02-09 13:27:21 +0100

    TUnit test framework

---
+++ Plugins/Reqnroll.TUnit.Generator.ReqnrollPlugin/GeneratorPlugin.cs
@@ -0,0 +1,16 @@
+﻿using Reqnroll.Generator.Plugins;
+using Reqnroll.Infrastructure;
+using Reqnroll.UnitTestProvider;
+
+[assembly: GeneratorPlugin(typeof(Reqnroll.TUnit.Generator.ReqnrollPlugin.GeneratorPlugin))]
+
+namespace Reqnroll.TUnit.Generator.ReqnrollPlugin
+{
+    public class GeneratorPlugin : IGeneratorPlugin
+    {
+        public void Initialize(GeneratorPluginEvents generatorPluginEvents, GeneratorPluginParameters generatorPluginParameters, UnitTestProviderConfiguration unitTestProviderConfiguration)
+        {
+            unitTestProviderConfiguration.UseUnitTestProvider("tunit");
+        }
+    }
+}
