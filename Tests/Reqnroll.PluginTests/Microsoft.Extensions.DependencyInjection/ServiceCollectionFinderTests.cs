c75c8e320b17593ef9951e30cb4f0d3bb31a7f27
Julian Verdurmen <5808377+304NotModified@users.noreply.github.com>
2025-02-20 17:45:15 +0100

    Added some more tests

--- Tests/Reqnroll.PluginTests/Microsoft.Extensions.DependencyInjection/ServiceCollectionFinderTests.cs
+++ Tests/Reqnroll.PluginTests/Microsoft.Extensions.DependencyInjection/ServiceCollectionFinderTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Linq;
+using System.Reflection;
 using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
@@ -13,11 +15,7 @@ public class ServiceCollectionFinderTests
     public void GetServiceCollection_HappyPath_ResolvesCorrectServiceCollection()
     {
         // Arrange
-        var assemblyMock = new Mock<Assembly>();
-        assemblyMock.Setup(m => m.GetTypes()).Returns([typeof(ValidStart1)]);
-
-        var testRunnerManagerMock = new Mock<ITestRunnerManager>();
-        testRunnerManagerMock.Setup(m => m.BindingAssemblies).Returns([assemblyMock.Object]);
+        var testRunnerManagerMock = CreateTestRunnerManagerMock(typeof(ValidStartWithAutoRegister));
         var sut = new ServiceCollectionFinder(testRunnerManagerMock.Object);
 
         // Act
@@ -27,7 +25,47 @@ public class ServiceCollectionFinderTests
         serviceCollection.Should().NotBeNull();
         var serviceProvider = serviceCollection.BuildServiceProvider();
         var testService = serviceProvider.GetRequiredService<ITestInterface>();
-        testService.Name.Should().Be("ValidStart1");
+        testService.Name.Should().Be("ValidStartWithAutoRegister");
+    }
+    
+    [Fact]
+    public void GetServiceCollection_AutoRegisterBindingsTrue_RegisterBindingsAsScoped()
+    {
+        // Arrange
+        var testRunnerManagerMock = CreateTestRunnerManagerMock(typeof(ValidStartWithAutoRegister), typeof(Binding1));
+        var sut = new ServiceCollectionFinder(testRunnerManagerMock.Object);
+
+        // Act
+        var (serviceCollection, _) = sut.GetServiceCollection();
+
+        // Assert
+        serviceCollection.Should().NotBeNull();
+        serviceCollection.Should().Contain(d => d.ImplementationType == typeof(Binding1) && d.Lifetime == ServiceLifetime.Scoped);
+    }
+
+    [Fact]
+    public void GetServiceCollection_AutoRegisterBindingsFalse_DoNotRegisterBindings()
+    {
+        // Arrange
+        var testRunnerManagerMock = CreateTestRunnerManagerMock(typeof(ValidStartWithoutAutoRegister), typeof(Binding1));
+        var sut = new ServiceCollectionFinder(testRunnerManagerMock.Object);
+
+        // Act
+        var (serviceCollection, _) = sut.GetServiceCollection();
+
+        // Assert
+        serviceCollection.Should().NotBeNull();
+        serviceCollection.Should().NotContain(d => d.ImplementationType == typeof(Binding1));
+    }
+
+    private static Mock<ITestRunnerManager> CreateTestRunnerManagerMock(params Type[] types)
+    {
+        var assemblyMock = new Mock<Assembly>();
+        assemblyMock.Setup(m => m.GetTypes()).Returns(types.ToArray());
+
+        var testRunnerManagerMock = new Mock<ITestRunnerManager>();
+        testRunnerManagerMock.Setup(m => m.BindingAssemblies).Returns([assemblyMock.Object]);
+        return testRunnerManagerMock;
     }
 
     private interface ITestInterface
@@ -37,15 +75,25 @@ public class ServiceCollectionFinderTests
 
     private record TestInterface(string Name) : ITestInterface;
 
-    private class ValidStart1
+    private class ValidStartWithAutoRegister
     {
         [ScenarioDependencies]
         public static IServiceCollection GetServices()
         {
             var serviceCollection = new ServiceCollection();
-            serviceCollection.AddSingleton<ITestInterface>(new TestInterface("ValidStart1"));
+            serviceCollection.AddSingleton<ITestInterface>(new TestInterface("ValidStartWithAutoRegister"));
             return serviceCollection;
         }
     }
-
+    private class ValidStartWithoutAutoRegister
+    {
+        [ScenarioDependencies(AutoRegisterBindings = false)]
+        public static IServiceCollection GetServices()
+        {
+            var serviceCollection = new ServiceCollection();
+            return serviceCollection;
+        }
+    }
+    [Binding]
+    private class Binding1;
 }
