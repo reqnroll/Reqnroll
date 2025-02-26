87bb565dd2f16557749d4bc41a2c285de080c2a9
Paul Turner <programming.hero@gmail.com>
2025-02-26 22:29:11 +0000

    Add generator scaffold

---
+++ Reqnroll.StepBindingSourceGenerator/CSharpStepBindingGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Reqnroll.StepBindingSourceGenerator;
+
+[Generator(LanguageNames.CSharp)]
+public class CSharpStepBindingGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
