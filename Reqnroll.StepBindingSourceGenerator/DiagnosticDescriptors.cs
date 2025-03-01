c170a9de6dfc25cea0528acc5eff743bb125d85d
Paul Turner <programming.hero@gmail.com>
2025-03-01 22:34:16 +0000

    Initial generator implementation

---
+++ Reqnroll.StepBindingSourceGenerator/DiagnosticDescriptors.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Reqnroll.StepBindingSourceGenerator;
+
+internal static class DiagnosticDescriptors
+{
+    private static LocalizableResourceString Resource(string name) => 
+        new(name, DiagnosticResources.ResourceManager, typeof(DiagnosticResources));
+
+    public static DiagnosticDescriptor ErrorStepTextCannotBeEmpty { get; } = new DiagnosticDescriptor(
+        "Reqnroll.1000",
+        Resource("ErrorStepTextCannotBeEmptyTitle"),
+        Resource("ErrorStepTextCannotBeEmptyMessage"),
+        "Reqnroll.Binding",
+        DiagnosticSeverity.Error,
+        true);
+}
---
+++ Reqnroll.StepBindingSourceGenerator/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Reqnroll.StepBindingSourceGenerator;
+
+internal static class EnumerableExtensions
+{
+    public static int GetSequenceHashCode<T>(this IEnumerable<T> values)
+    {
+        unchecked
+        {
+            var hash = 4037797;
+
+            foreach (var item in values)
+            {
+                hash *= 1864987 + item?.GetHashCode() ?? 0;
+            }
+
+            return hash;
+        }
+    }
+}
