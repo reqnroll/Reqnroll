0f1315ac35cb054c3c2484330d58bc31393c9cb9
Paul Turner <programming.hero@gmail.com>
2025-03-05 23:59:42 +0000

    Fixed up diagnostics for step bindings

--- Reqnroll.StepBindingSourceGenerator/DiagnosticDescriptors.cs
+++ Reqnroll.StepBindingSourceGenerator/DiagnosticDescriptors.cs
@@ -7,11 +7,43 @@ internal static class DiagnosticDescriptors
     private static LocalizableResourceString Resource(string name) => 
         new(name, DiagnosticResources.ResourceManager, typeof(DiagnosticResources));
 
-    public static DiagnosticDescriptor ErrorStepTextCannotBeEmpty { get; } = new DiagnosticDescriptor(
+    public static DiagnosticDescriptor ErrorStepMethodMustReturnVoidOrTask { get; } = new DiagnosticDescriptor(
         "Reqnroll1000",
+        Resource("ErrorStepMethodMustReturnVoidOrTaskTitle"),
+        Resource("ErrorStepMethodMustReturnVoidOrTaskMessage"),
+        "Reqnroll.Binding",
+        DiagnosticSeverity.Error,
+        true);
+
+    public static DiagnosticDescriptor ErrorAsyncStepMethodMustReturnTask { get; } = new DiagnosticDescriptor(
+        "Reqnroll1001",
+        Resource("ErrorAsyncStepMethodMustReturnTaskTitle"),
+        Resource("ErrorAsyncStepMethodMustReturnTaskMessage"),
+        "Reqnroll.Binding",
+        DiagnosticSeverity.Error,
+        true);
+
+    public static DiagnosticDescriptor ErrorStepTextCannotBeEmpty { get; } = new DiagnosticDescriptor(
+        "Reqnroll1002",
         Resource("ErrorStepTextCannotBeEmptyTitle"),
         Resource("ErrorStepTextCannotBeEmptyMessage"),
         "Reqnroll.Binding",
         DiagnosticSeverity.Error,
+        true);
+
+    public static DiagnosticDescriptor WarningStepTextHasLeadingWhitespace { get; } = new DiagnosticDescriptor(
+        "Reqnroll1003",
+        Resource("WarningStepTextHasLeadingWhitespaceTitle"),
+        Resource("WarningStepTextHasLeadingWhitespaceMessage"),
+        "Reqnroll.Binding",
+        DiagnosticSeverity.Warning,
+        true);
+
+    public static DiagnosticDescriptor WarningStepTextHasTrailingWhitespace { get; } = new DiagnosticDescriptor(
+        "Reqnroll1004",
+        Resource("WarningStepTextHasTrailingWhitespaceTitle"),
+        Resource("WarningStepTextHasTrailingWhitespaceMessage"),
+        "Reqnroll.Binding",
+        DiagnosticSeverity.Warning,
         true);
 }
--- Tests/Reqnroll.StepBindingSourceGeneratorTests/StepAnalysisTests.cs
+++ Tests/Reqnroll.StepBindingSourceGeneratorTests/StepAnalysisTests.cs
@@ -1,10 +1,140 @@
 ﻿using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 
 namespace Reqnroll.StepBindingSourceGenerator;
 
 public class StepAnalysisTests
 {
+    [Fact]
+    public void StepBindingOnMethodWithReturnValue_GeneratesErrorStepMethodMustReturnVoidOrTask()
+    {
+        var source = MarkedSourceText.Parse(
+            """
+            using Reqnroll;
+            
+            namespace Sample.Tests;
+            
+            [Binding]
+            public class GameSteps
+            {
+                [When]
+                public string ⇥WhenMakerStartsAGame⇤()
+                {
+                    return "Game started";
+                }
+            }
+            """);
+
+        var result = GeneratorDriver.RunGenerator(source, "/spec/Sample.feature");
+
+        result.Diagnostics.Should().BeEquivalentTo(
+            [
+                Diagnostic.Create(
+                    DiagnosticDescriptors.ErrorStepMethodMustReturnVoidOrTask,
+                    source.GetMarkedLocation("/spec/Sample.feature"),
+                    "GameSteps.WhenmakerStartsAGame",
+                    "void")
+            ]);
+
+        result.GeneratedTrees.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StepBindingOnAsyncMethodWithVoidReturn_GeneratesErrorAsyncStepMethodMustReturnTask()
+    {
+        var source = MarkedSourceText.Parse(
+            """
+            using System.Threading.Tasks;
+            using Reqnroll;
+            
+            namespace Sample.Tests;
+            
+            [Binding]
+            public class GameSteps
+            {
+                [When]
+                public async void ⇥WhenMakerStartsAGame⇤()
+                {
+                    await Task.Delay(100);
+                }
+            }
+            """);
+
+        var result = GeneratorDriver.RunGenerator(source, "/spec/Sample.feature");
+
+        result.Diagnostics.Should().BeEquivalentTo(
+            [
+                Diagnostic.Create(
+                    DiagnosticDescriptors.ErrorAsyncStepMethodMustReturnTask,
+                    source.GetMarkedLocation("/spec/Sample.feature"),
+                    "GameSteps.WhenMakerStartsAGame",
+                    "async")
+            ]);
+
+        result.GeneratedTrees.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StepBindingWithNullText_GeneratesErrorStepTextCannotBeEmpty()
+    {
+        var source = MarkedSourceText.Parse(
+            """
+            using Reqnroll;
+
+            namespace Sample.Tests;
+
+            [Binding]
+            public class GameSteps
+            {
+                [When(⇥null⇤)]
+                public void WhenMakerStartsAGame()
+                {
+                }
+            }
+            """);
+
+        var result = GeneratorDriver.RunGenerator(source, "/spec/Sample.feature");
+
+        result.Diagnostics.Should().BeEquivalentTo(
+            [
+                Diagnostic.Create(
+                    DiagnosticDescriptors.ErrorStepTextCannotBeEmpty,
+                    source.GetMarkedLocation("/spec/Sample.feature"))
+            ]);
+
+        result.GeneratedTrees.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void StepBindingWithDefaultText_GeneratesErrorStepTextCannotBeEmpty()
+    {
+        var source = MarkedSourceText.Parse(
+            """
+            using Reqnroll;
+
+            namespace Sample.Tests;
+
+            [Binding]
+            public class GameSteps
+            {
+                [When(⇥default⇤)]
+                public void WhenMakerStartsAGame()
+                {
+                }
+            }
+            """);
+
+        var result = GeneratorDriver.RunGenerator(source, "/spec/Sample.feature");
+
+        result.Diagnostics.Should().BeEquivalentTo(
+            [
+                Diagnostic.Create(
+                    DiagnosticDescriptors.ErrorStepTextCannotBeEmpty,
+                    source.GetMarkedLocation("/spec/Sample.feature"))
+            ]);
+
+        result.GeneratedTrees.Should().BeEmpty();
+    }
+
     [Fact]
     public void StepBindingWithEmptyText_GeneratesErrorStepTextCannotBeEmpty()
     {
@@ -24,34 +154,115 @@ public class StepAnalysisTests
             }
             """);
 
-        var syntaxTree = CSharpSyntaxTree.ParseText(source, path: "/spec/Sample.feature");
-
-        syntaxTree.GetDiagnostics().Should().BeEmpty();
-
-        var references = AppDomain.CurrentDomain
-            .GetAssemblies()
-            .Where(assembly => !assembly.IsDynamic)
-            .Select(assembly => MetadataReference.CreateFromFile(assembly.Location))
-            .Cast<MetadataReference>()
-            .ToList();
-
-        references.Add(MetadataReference.CreateFromFile(typeof(WhenAttribute).Assembly.Location));
-
-        var compilation = CSharpCompilation.Create(
-            "SourceGeneratorTests",
-            [ syntaxTree ],
-            references,
-            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
-
-        compilation.GetDiagnostics().Should().BeEmpty();
-
-        var generator = new CSharpStepBindingGenerator();
-        var result = CSharpGeneratorDriver.Create(generator).RunGenerators(compilation).GetRunResult();
+        var result = GeneratorDriver.RunGenerator(source, "/spec/Sample.feature");
 
         result.Diagnostics.Should().BeEquivalentTo(
             [
                 Diagnostic.Create(
                     DiagnosticDescriptors.ErrorStepTextCannotBeEmpty,
+                    source.GetMarkedLocation("/spec/Sample.feature"))
+            ]);
+
+        result.GeneratedTrees.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    [InlineData("    ")]
+    public void StepBindingWithWhitespaceText_GeneratesErrorStepTextCannotBeEmpty(string whitespace)
+    {
+        var source = MarkedSourceText.Parse(
+            $$"""
+            using Reqnroll;
+
+            namespace Sample.Tests;
+
+            [Binding]
+            public class GameSteps
+            {
+                [When(⇥"{{whitespace}}"⇤)]
+                public void WhenMakerStartsAGame()
+                {
+                }
+            }
+            """);
+
+        var result = GeneratorDriver.RunGenerator(source, "/spec/Sample.feature");
+
+        result.Diagnostics.Should().BeEquivalentTo(
+            [
+                Diagnostic.Create(
+                    DiagnosticDescriptors.ErrorStepTextCannotBeEmpty,
+                    source.GetMarkedLocation("/spec/Sample.feature"))
+            ]);
+
+        result.GeneratedTrees.Should().BeEmpty();
+    }
+
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    [InlineData("    ")]
+    public void StepBindingWithLeadingWhitespace_GeneratesWarningStepTextHasLeadingWhitespace(string whitespace)
+    {
+        var source = MarkedSourceText.Parse(
+            $$"""
+            using Reqnroll;
+
+            namespace Sample.Tests;
+
+            [Binding]
+            public class GameSteps
+            {
+                [When("⇥{{whitespace}}⇤Maker starts a game")]
+                public void WhenMakerStartsAGame()
+                {
+                }
+            }
+            """);
+
+        var result = GeneratorDriver.RunGenerator(source, "/spec/Sample.feature");
+
+        result.Diagnostics.Should().BeEquivalentTo(
+            [
+                Diagnostic.Create(
+                    DiagnosticDescriptors.WarningStepTextHasLeadingWhitespace,
+                    source.GetMarkedLocation("/spec/Sample.feature"))
+            ]);
+
+        result.GeneratedTrees.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("\t")]
+    [InlineData("    ")]
+    public void StepBindingWithTrailingWhitespace_GeneratesWarningStepTextHasTrailingWhitespace(string whitespace)
+    {
+        var source = MarkedSourceText.Parse(
+            $$"""
+            using Reqnroll;
+
+            namespace Sample.Tests;
+
+            [Binding]
+            public class GameSteps
+            {
+                [When("Maker starts a game⇥{{whitespace}}⇤")]
+                public void WhenMakerStartsAGame()
+                {
+                }
+            }
+            """);
+
+        var result = GeneratorDriver.RunGenerator(source, "/spec/Sample.feature");
+
+        result.Diagnostics.Should().BeEquivalentTo(
+            [
+                Diagnostic.Create(
+                    DiagnosticDescriptors.WarningStepTextHasTrailingWhitespace,
                     source.GetMarkedLocation("/spec/Sample.feature"))
             ]);
 
