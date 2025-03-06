7d0c4cac6e84fa0a9a8b32d354731b6e4eb5f58c
Twan Jacobs <AroglDarthu@users.noreply.github.com>
2025-03-06 11:34:06 +0100

    Ideas/457 map table directly to constructor for stepargumenttransformers (#488)
    * First attempt
    
    * Add tests and fix logic error
    
    * Prove odd behaviour of GetConstructorMatchingToColumnNames
    
    * Refactor handling of instance creation

---
+++ Tests/Reqnroll.RuntimeTests/AssistTests/ExampleEntities/ProductCreatedMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reqnroll.RuntimeTests.AssistTests.ExampleEntities;
+
+public sealed class ProductCreatedMessage : AbstractMessage<ProductCreatedMessageContent>
+{
+    public ProductCreatedMessage(DateTimeOffset messageCreatedAt, string productCode, string productName, DateTime startOfSale)
+    {
+        MessageCreatedAt = messageCreatedAt;
+        MessageContent = new ProductCreatedMessageContent(productCode, productName, startOfSale);
+    }
+}
