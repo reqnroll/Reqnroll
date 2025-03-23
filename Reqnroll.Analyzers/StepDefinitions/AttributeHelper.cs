61d8e41f801c857b90bf89878775cae72f614bba
Paul Turner <programming.hero@gmail.com>
2025-03-23 14:35:50 +0000

    Add initial analyzer for step text

---
+++ Reqnroll.Analyzers/StepDefinitions/AttributeHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace Reqnroll.Analyzers.StepDefinitions;
+
+internal static class AttributeHelper
+{
+    private static readonly HashSet<string> StepAttributeTypes =
+    [
+        "Reqnroll.GivenAttribute",
+        "Reqnroll.WhenAttribute",
+        "Reqnroll.ThenAttribute",
+        "Reqnroll.StepDefinitionAttribute"
+    ];
+
+    private static bool IsStepAttributeType(string qualifiedTypeName) => StepAttributeTypes.Contains(qualifiedTypeName);
+
+    public static bool IsStepAttribute(AttributeData attributeData)
+    {
+        if (attributeData.AttributeClass == null)
+        {
+            return false;
+        }
+
+        return IsStepAttributeType(attributeData.AttributeClass.ToDisplayString());
+    }
+
+    public static bool IsStepAttribute(ISymbol symbol)
+    {
+        return IsStepAttributeType(symbol.ContainingType.ToDisplayString());
+    }
+}
