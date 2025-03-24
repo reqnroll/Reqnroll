9d2f7a22a8a1ad5e2bed4d25d0c2de6a87f72659
Harish Hareendra Babu <harish.babu@codurance.com>
2025-03-24 16:25:16 +0000

    feat: xunit3 plugin and generator

---
+++ Plugins/Reqnroll.xUnit3.ReqnrollPlugin/XUnit3TraceListener.cs
@@ -0,0 +1,17 @@
+using System;
+using Reqnroll.Tracing;
+
+namespace Reqnroll.xUnit3.ReqnrollPlugin;
+
+public class XUnit3TraceListener : ITraceListener
+{
+    public void WriteTestOutput(string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void WriteToolOutput(string message)
+    {
+        throw new NotImplementedException();
+    }
+}
