5b3a5a8ab04681e08307999825ade0348f20a170
Harish Hareendra Babu <harish.babu@codurance.com>
2025-03-27 16:50:45 +0000

    fix: xunit v3 - throw on pending test exception

--- Plugins/Reqnroll.xUnit3.ReqnrollPlugin/XUnit3RuntimeProvider.cs
+++ Plugins/Reqnroll.xUnit3.ReqnrollPlugin/XUnit3RuntimeProvider.cs
@@ -1,12 +1,20 @@
+using System;
+using System.Runtime.Serialization;
 using Reqnroll.UnitTestProvider;
+using Xunit.Sdk;
 
 namespace Reqnroll.xUnit3.ReqnrollPlugin;
 
 public class XUnit3RuntimeProvider : IUnitTestRuntimeProvider
 {
-    public void TestPending(string message) => Xunit.Assert.Skip(message);
+    public void TestPending(string message) => throw new XUnitPendingException($"Test pending: {message}");
 
     public void TestInconclusive(string message) => throw new XUnitInconclusiveException($"Test inconclusive: {message}");
 
     public void TestIgnore(string message) => Xunit.Assert.Skip(message);
+}
+
+[Serializable]
+internal class XUnitPendingException(string message) : XunitException(message)
+{
 }
