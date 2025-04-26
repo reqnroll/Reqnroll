73d28399102e61fb7e1879cd29768b27164e89bd
Adam Pluciński <adaskothebeast@gmail.com>
2025-04-26 18:36:36 +0200

    ModuleInitializer attribute for netstandard2.0

---
+++ Plugins/Reqnroll.TUnit.ReqnrollPlugin/ModuleInitializerAttribute.cs
@@ -0,0 +1,6 @@
+﻿#if NETSTANDARD2_0
+namespace System.Runtime.CompilerServices;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public sealed class ModuleInitializerAttribute : Attribute { }
+#endif
