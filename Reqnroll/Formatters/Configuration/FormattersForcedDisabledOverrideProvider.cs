7daea10f1c9ed2f2c7a34dbfb12ba3292d0d10c6
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2025-07-21 09:23:06 -0500

    Add FormattersForcedDisabledOverrideProvider class
    This commit introduces the `FormattersForcedDisabledOverrideProvider` class, which implements the `IFormattersConfigurationDisableOverrideProvider` interface and always returns `true` for the `Disabled` method.
    
    Additionally, the `BindingProviderService.cs` file has been updated to register this new provider in the dependency injection container so that the formatters do not run when only binding discovery is being run.

---
+++ Reqnroll/Formatters/Configuration/FormattersForcedDisabledOverrideProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Reqnroll.Formatters.Configuration;
+
+public class FormattersForcedDisabledOverrideProvider : IFormattersConfigurationDisableOverrideProvider
+{
+    public bool Disabled()
+    {
+        return true;
+    }
+}
