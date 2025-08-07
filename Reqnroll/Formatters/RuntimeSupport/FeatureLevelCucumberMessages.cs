450ee05ea3b531e64588e857692f33293daee7ba
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2025-08-07 16:14:02 -0500

    Refactor FeatureLevelCucumberMessages for lazy loading
    Implement `Lazy<T>` for properties `Source`, `GherkinDocument`, and `Pickles` to enable lazy initialization.

--- Reqnroll/Formatters/RuntimeSupport/FeatureLevelCucumberMessages.cs
+++ Reqnroll/Formatters/RuntimeSupport/FeatureLevelCucumberMessages.cs
@@ -1,4 +1,4 @@
-﻿using Io.Cucumber.Messages.Types;
+using Io.Cucumber.Messages.Types;
 using System;
 using System.Collections.Generic;
 
@@ -10,8 +10,12 @@ namespace Reqnroll.Formatters.RuntimeSupport;
 /// </summary>
 public class FeatureLevelCucumberMessages(Func<Source> source, Func<GherkinDocument> gherkinDocument, Func<IEnumerable<Pickle>> pickles)
 {
+    private Lazy<Source> _source = new Lazy<Source>(source);
+    private Lazy<GherkinDocument> _gherkinDocument = new Lazy<GherkinDocument>(gherkinDocument);
+    private Lazy<IEnumerable<Pickle>> _pickles = new Lazy<IEnumerable<Pickle>>(pickles);
+
     public bool HasMessages => Source is not null && GherkinDocument is not null && Pickles is not null;
-    public Source Source { get; } = source();
-    public GherkinDocument GherkinDocument { get; } = gherkinDocument();
-    public IEnumerable<Pickle> Pickles { get; } = pickles();
+    public Source Source { get => _source.Value; }
+    public GherkinDocument GherkinDocument { get => _gherkinDocument.Value; }
+    public IEnumerable<Pickle> Pickles { get => _pickles.Value; }
 }
