84cc30f3d8dcf9aceaaca71f7117bf4b61c4f539
Julian Verdurmen <304NotModified@users.noreply.github.com>
2025-08-09 17:58:28 +0200

    Remove [Serializable] from exceptions, including serialisation constructors

--- Plugins/Reqnroll.Microsoft.Extensions.DependencyInjection.ReqnrollPlugin/InvalidScenarioDependenciesException.cs
+++ Plugins/Reqnroll.Microsoft.Extensions.DependencyInjection.ReqnrollPlugin/InvalidScenarioDependenciesException.cs
@@ -2,7 +2,6 @@
 
 namespace Reqnroll.Microsoft.Extensions.DependencyInjection;
 
-[Serializable]
 public class InvalidScenarioDependenciesException(string reason) : ReqnrollException("[ScenarioDependencies] should return IServiceCollection but " + reason)
 {
     public override string HelpLink { get; set; } = "https://go.reqnroll.net/doc-msdi";
--- Reqnroll/ErrorHandling/MissingStepDefinitionException.cs
+++ Reqnroll/ErrorHandling/MissingStepDefinitionException.cs
@@ -1,21 +1,12 @@
 using System;
-using System.Runtime.Serialization;
 
 // the exceptions are part of the public API, keep them in Reqnroll namespace
 namespace Reqnroll
 {
-    [Serializable]
     public class MissingStepDefinitionException : ReqnrollException
     {
         public MissingStepDefinitionException()
             : base("No matching step definition found for one or more steps.")
-        {
-        }
-
-        protected MissingStepDefinitionException(
-            SerializationInfo info,
-            StreamingContext context)
-            : base(info, context)
         {
         }
     }
