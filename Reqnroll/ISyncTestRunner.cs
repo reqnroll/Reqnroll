df953b826321cbd742597065a45a7f1fb7195eb6
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-08-12 22:06:22 +0200

    Update Reqnroll/ISyncTestRunner.cs
    Co-authored-by: Julian Verdurmen <5808377+304NotModified@users.noreply.github.com>

--- Reqnroll/ISyncTestRunner.cs
+++ Reqnroll/ISyncTestRunner.cs
@@ -2,5 +2,5 @@ using System;
 
 namespace Reqnroll;
 
-[Obsolete("The synchronous test runner API has been deprecated. Please use ITestRunner instead.", true)]
+[Obsolete("The synchronous test runner API has been deprecated. Please use ITestRunner instead. Interface will be removed in v4.", true)]
 public interface ISyncTestRunner;
