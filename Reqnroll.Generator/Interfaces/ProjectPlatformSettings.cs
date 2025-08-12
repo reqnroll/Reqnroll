fff41d523c23cd9d7a325270fbfe29076e4702ce
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-08-12 17:29:34 +0200

    Cleanup the generator interface code and remove old code-behind generation stuff, like [Serializable]

--- Reqnroll.Generator/Interfaces/ProjectPlatformSettings.cs
+++ Reqnroll.Generator/Interfaces/ProjectPlatformSettings.cs
@@ -1,43 +1,9 @@
-using System;
+namespace Reqnroll.Generator.Interfaces;
 
-namespace Reqnroll.Generator.Interfaces
+public class ProjectPlatformSettings
 {
-    /// IMPORTANT
-    /// This class is used for interop with the Visual Studio Extension
-    /// DO NOT REMOVE OR RENAME FIELDS!
-    /// This breaks binary serialization across appdomains
-    [Serializable]
-    public class ProjectPlatformSettings
-    {
-        /// <summary>
-        /// Specifies the programming language of the project. Optional, defaults to C# 3.0.
-        /// </summary>
-        public string Language { get; set; }
-        /// <summary>
-        /// The version of the <see cref="Language"/>. Optional, defaults to C# 3.0.
-        /// </summary>
-        [Obsolete("Not used anymore, will be removed with Reqnroll future versions")]
-        public Version LanguageVersion { get; set; }
-
-        /// <summary>
-        /// Specifies the target platform of the project. Optional, defaults to .NET 3.5.
-        /// </summary>
-        [Obsolete("Not used anymore, will be removed with Reqnroll future versions")]
-        public string Platform { get; set; }
-        /// <summary>
-        /// The version of the <see cref="Platform"/>. Optional, defaults to .NET 3.5.
-        /// </summary>
-        [Obsolete("Not used anymore, will be removed with Reqnroll future versions")]
-        public Version PlatformVersion { get; set; }
-
-        public ProjectPlatformSettings()
-        {
-            Language = GenerationTargetLanguage.CSharp;
-#pragma warning disable 618
-            LanguageVersion = new Version(3, 0);
-            Platform = GenerationTargetPlatform.DotNet;
-            PlatformVersion = new Version(3, 5);
-#pragma warning restore 618
-        }
-    }
+    /// <summary>
+    /// Specifies the programming language of the project. Optional, defaults to C# 3.0.
+    /// </summary>
+    public string Language { get; set; } = GenerationTargetLanguage.CSharp;
 }
