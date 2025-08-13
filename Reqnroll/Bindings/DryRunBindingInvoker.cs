8be0723c3ef0ad0094ed3a07c5771f212eae8841
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-08-13 14:22:58 +0200

    applied review suggestions

--- Reqnroll/Bindings/DryRunBindingInvoker.cs
+++ Reqnroll/Bindings/DryRunBindingInvoker.cs
@@ -7,12 +7,6 @@ namespace Reqnroll.Bindings;
 
 public class DryRunBindingInvoker : IAsyncBindingInvoker
 {
-    public object InvokeBinding(IBinding binding, IContextManager contextManager, object[] arguments, ITestTracer testTracer, out TimeSpan duration)
-    {
-        duration = TimeSpan.Zero;
-        return null;
-    }
-
     public Task<object> InvokeBindingAsync(IBinding binding, IContextManager contextManager, object[] arguments, ITestTracer testTracer, DurationHolder durationHolder)
     {
         durationHolder.Duration = TimeSpan.Zero;
