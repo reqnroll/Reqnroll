ce372d6fc600d1eecfc1b92d22aa5c51f1cf2eef
Paul Turner <programming.hero@gmail.com>
2025-08-20 00:16:21 +0100

    Make table escape sequences match documented support

--- Reqnroll.CodeAnalysis.Gherkin/TableLiteralEscapingStyle.cs
+++ Reqnroll.CodeAnalysis.Gherkin/TableLiteralEscapingStyle.cs
@@ -4,34 +4,79 @@ using System.Text;
 
 namespace Reqnroll.CodeAnalysis.Gherkin;
 
-internal class TableLiteralEscapingStyle : DefaultLiteralEscapingStyle
+internal class TableLiteralEscapingStyle : LiteralEscapingStyle
 {
     protected override bool RequiresEscaping(char c)
     {
-        // In the context of a table, we also need to escape the pipe character '|'
-        return c == '|' || base.RequiresEscaping(c);
+        // In the context of a table, we only escape:
+        // - The pipe character '|', which would otherwise separate columns.
+        // - The newline character '\n' which would otherwise start a new row.
+        // - The backslash character '\' to separate it from other escape sequences.
+        return c is '|' or '\n' or '\\';
     }
 
     protected override bool TryEncode(char c, [NotNullWhen(true)] out string? encoded)
     {
-        if (c == '|')
+        encoded = null;
+
+        switch (c)
         {
-            encoded = "\\|";
-            return true;
+            case '\\':
+                encoded = "\\\\";
+                break;
+            case '\n':
+                encoded = "\\n";
+                break;
+            case '|':
+                encoded = "\\|";
+                break;
         }
 
-        return base.TryEncode(c, out encoded);
+        return encoded is not null;
     }
 
-    protected override bool TryUnescapeSequence(SourceTextSpan sourceTextSpan, StringBuilder stringBuilder, out int consumed)
+    public override string Unescape(SourceTextSpan value)
     {
-        if (sourceTextSpan[0] == '|')
+        var sb = new StringBuilder();
+
+        for (var i = 0; i < value.Length; i++)
         {
-            stringBuilder.Append('|');
-            consumed = 1;
-            return true;
+            var c = value[i];
+
+            if (c == '\\' && i + 1 < value.Length && TryUnescape(value[i + 1], sb))
+            {
+                // Read the escaped sequence into the buffer.
+                i++;
+            }
+            else
+            {
+                // Copy the value literally.
+                sb.Append(c);
+            }
         }
 
-        return base.TryUnescapeSequence(sourceTextSpan, stringBuilder, out consumed);
+        return sb.ToString();
+    }
+
+    private static bool TryUnescape(char c, StringBuilder sb)
+    {
+        switch (c)
+        {
+            case '\\':
+                sb.Append('\\');
+                return true;
+
+            case 'n':
+                sb.Append('\n');
+                return true;
+
+            case '|':
+                sb.Append('|');
+                return true;
+
+            default:
+                // If it's not a recognized escape sequence, we don't unescape it.
+                return false;
+        }
     }
 }
