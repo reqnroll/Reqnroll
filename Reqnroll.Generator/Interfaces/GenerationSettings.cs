13092c63d28e2364486cbf2fb58586192f5ae42c
Gáspár Nagy <gaspar.nagy@gmail.com>
2025-09-26 11:08:29 +0200

    Added comment to GenerationSettings

--- Reqnroll.Generator/Interfaces/GenerationSettings.cs
+++ Reqnroll.Generator/Interfaces/GenerationSettings.cs
@@ -1,3 +1,6 @@
 namespace Reqnroll.Generator.Interfaces;
 
+/// <summary>
+/// Placeholder class for generation settings. Currently, not in use. We can consider to remove it in v4.
+/// </summary>
 public class GenerationSettings;
