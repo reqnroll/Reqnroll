3e819867946ddb167af2f13707c135ac0c44b2b7
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2025-10-07 09:15:43 -0500

    Performance: Cache the value of IsDryRun environment variable (#874)
    * Performance: Cache the value of IsDryRun environment variable as it is evaluated frequently.
    
    * PR suggestion (#878)
    
    Co-authored-by: Steven Ryland <stevenryland@microsoft.com>
    
    ---------
    
    Co-authored-by: Steven Ryland <stevenry@outlook.com>
    Co-authored-by: Steven Ryland <stevenryland@microsoft.com>

--- Reqnroll/EnvironmentAccess/EnvironmentOptions.cs
+++ Reqnroll/EnvironmentAccess/EnvironmentOptions.cs
@@ -1,4 +1,5 @@
-﻿#nullable enable
+#nullable enable
+using System;
 using System.Collections.Generic;
 using Reqnroll.CommonModels;
 
@@ -13,10 +14,12 @@ public class EnvironmentOptions(IEnvironmentWrapper environment) : IEnvironmentO
     public const string REQNROLL_BINDING_OUTPUT_ENVIRONMENT_VARIABLE = "REQNROLL_BINDING_OUTPUT";
     public const string DOTNET_RUNNING_IN_CONTAINER_ENVIRONMENT_VARIABLE = "DOTNET_RUNNING_IN_CONTAINER";
 
-    public bool IsDryRun => 
+    private readonly Lazy<bool> _isDryRunLazy = new Lazy<bool>(() =>
         environment.GetEnvironmentVariable(REQNROLL_DRY_RUN_ENVIRONMENT_VARIABLE) is ISuccess<string> dryRunVar
             && bool.TryParse(dryRunVar.Result, out bool isDryRun)
-            && isDryRun;
+            && isDryRun);
+
+    public bool IsDryRun => _isDryRunLazy.Value;
 
     public string? BindingsOutputFilepath => 
         environment.GetEnvironmentVariable(REQNROLL_BINDING_OUTPUT_ENVIRONMENT_VARIABLE) is ISuccess<string> outputVar
