71e6fcc34de716659afa64f9b99e830d22a7f7cc
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2025-11-10 11:19:09 -0600

    File scope namespace for the ReqnrollTagExpressionParser.

--- Reqnroll/Bindings/Discovery/ReqnrollTagExpressionParser.cs
+++ Reqnroll/Bindings/Discovery/ReqnrollTagExpressionParser.cs
@@ -3,34 +3,33 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Reqnroll.Bindings.Discovery
+namespace Reqnroll.Bindings.Discovery;
+
+public class ReqnrollTagExpressionParser(ITagExpressionParser tagExpressionParser) : ITagExpressionParser
 {
-    public class ReqnrollTagExpressionParser(ITagExpressionParser tagExpressionParser) : ITagExpressionParser
+    public ITagExpression Parse(string tagExpression)
     {
-        public ITagExpression Parse(string tagExpression)
-        {
-            return Rewrite(tagExpressionParser.Parse(tagExpression));
-        }
+        return Rewrite(tagExpressionParser.Parse(tagExpression));
+    }
 
-        private ITagExpression Rewrite(ITagExpression expression)
+    private ITagExpression Rewrite(ITagExpression expression)
+    {
+        return expression switch
         {
-            return expression switch
-            {
-                NullExpression nullExpression => nullExpression,
-                NotNode notNode => new NotNode(Rewrite(notNode.Operand)),
-                BinaryOpNode binaryOpNode => new BinaryOpNode(binaryOpNode.Op, Rewrite(binaryOpNode.Left), Rewrite(binaryOpNode.Right)),
-                LiteralNode literalNode => new LiteralNode(PrefixLiteral(literalNode.Name)),
-                _ => throw new NotSupportedException($"Unsupported tag expression type: {expression.GetType().FullName}"),
-            };
-        }
+            NullExpression nullExpression => nullExpression,
+            NotNode notNode => new NotNode(Rewrite(notNode.Operand)),
+            BinaryOpNode binaryOpNode => new BinaryOpNode(binaryOpNode.Op, Rewrite(binaryOpNode.Left), Rewrite(binaryOpNode.Right)),
+            LiteralNode literalNode => new LiteralNode(PrefixLiteral(literalNode.Name)),
+            _ => throw new NotSupportedException($"Unsupported tag expression type: {expression.GetType().FullName}"),
+        };
+    }
 
-        private string PrefixLiteral(string name)
-        {
-            if (name.IsNullOrEmpty() )
-                return name;
-            if (name.StartsWith("@"))
-                return name;
-            return "@" + name;
-        }
+    private string PrefixLiteral(string name)
+    {
+        if (name.IsNullOrEmpty() )
+            return name;
+        if (name.StartsWith("@"))
+            return name;
+        return "@" + name;
     }
 }
