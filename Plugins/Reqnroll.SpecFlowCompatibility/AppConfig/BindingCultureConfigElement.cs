f0217cc8de258b8adea29f449ce91c650fe3b864
Paul "Code Grump" Turner <programming.hero@gmail.com>
2025-11-26 12:31:46 +0000

    Switch to project-based packing (#914)
    * Switch primary project to use csproj-based packing
    
    * Customise xunit.v3 plugin
    
    * Add test framework adapter sdk
    
    * Simplify text replacement to not require custom task
    
    * Fix project to build in .NET Framework and Core
    
    * Fixed replacement of namespace in xunit plugin assembly hooks generation
    
    * Make system tests explicitly depend on packed projects
    
    * Complete generic SDK for test framework adapters
    
    * Migrate xunit to new structure
    
    * Migrate TUnit generator to MSBuild packaging
    
    * Migrate NUnit generator to MSBuild packaging
    
    * Migrate MSTest generator to MSBuild packaging
    
    * Fix issues with Specs project
    
    * Switch to MSBuild based token-replacement
    
    * Work around VS bug with single TFM to multi TFM project references
    
    * Migrate plugins from NuSpec files
    
    * Fix plugin locator tests to account for new packing conventions
    
    * Add SpecFlow Compatibility library - resolves packing shared code
    
    * Convert CustomPlugin to project-based packing
    
    * Fix issue with pack-based dependency management
    
    * Fix incorrect expectation in runtime plugin test
    
    * Made NuGetPackageVersion embedding outside of source-controlled files
    This makes it so the produced source file is included as intermediary output and will function with an up-to-date check so that MSBuild will skip it for the vast majority of builds, using the existing file.
    
    * Fix issues resolving NuGet packages in generated test projects
    
    * Fix typo in xunit property specification
    
    * Fix NUnit packing
    
    * Fix references to package-producing projects
    
    * Add inclusion of generator plugins to test framework adapters
    
    * Fix bug with generators referencing non-existant package
    
    * Restore deps.json files for generator plugins
    
    * Restore deps.json file for MSBuild Generation
    
    * Fix missing solution files
    
    * Fix deps packaging in generator plugins
    
    * Trim fat from generator plugins
    
    * Make deps.json include consistent with plugins
    
    * Remove diagnostic message
    
    * Add CHANGELOG entry
    
    ---------
    
    Co-authored-by: Gáspár Nagy <gaspar.nagy@gmail.com>

---
+++ Plugins/Reqnroll.SpecFlowCompatibility/AppConfig/BindingCultureConfigElement.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Configuration;
+
+namespace Reqnroll.SpecFlowCompatibility.AppConfig;
+
+public class BindingCultureConfigElement : ConfigurationElement
+{
+    [ConfigurationProperty("name", DefaultValue = "en-US", IsRequired = false)]
+    [RegexStringValidator(@"\w{2}(-\w{2})?")]
+    public string Name
+    {
+        get { return (String)this["name"]; }
+        set { this["name"] = value; }
+    }
+}
