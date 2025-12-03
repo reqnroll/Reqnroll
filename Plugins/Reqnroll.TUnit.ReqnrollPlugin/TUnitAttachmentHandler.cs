6cb12f04160c40fbb946a7f5ba8e14abbde0a25a
Adam Pluciński <adaskothebeast@gmail.com>
2025-12-04 00:48:27 +0100

    added comments

--- Plugins/Reqnroll.TUnit.ReqnrollPlugin/TUnitAttachmentHandler.cs
+++ Plugins/Reqnroll.TUnit.ReqnrollPlugin/TUnitAttachmentHandler.cs
@@ -5,12 +5,20 @@ using Reqnroll.Tracing;
 
 namespace Reqnroll.TUnit.ReqnrollPlugin;
 
+/// <summary>
+/// Handles test attachments for TUnit test framework.
+/// </summary>
 public class TUnitAttachmentHandler : ReqnrollAttachmentHandler
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TUnitAttachmentHandler"/> class.
+    /// </summary>
+    /// <param name="traceListener">The trace listener for fallback output.</param>
     public TUnitAttachmentHandler(ITraceListener traceListener) : base(traceListener)
     {
     }
 
+    /// <inheritdoc />
     public override void AddAttachment(string filePath)
     {
         try
