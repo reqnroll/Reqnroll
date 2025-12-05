58b8411aeb9b808b81e682a440156d31b535d46e
Paul Turner <programming.hero@gmail.com>
2025-12-05 22:49:06 +0000

    Clean up analyzer behaviour to match actual Reqnroll behaviour

--- Reqnroll.Analyzers/AnalyzerReleases.Unshipped.md
+++ Reqnroll.Analyzers/AnalyzerReleases.Unshipped.md
@@ -6,7 +6,7 @@
 Rule ID | Category | Severity | Notes
 --------|----------|----------|--------------------
 RR1001  |  Usage   |  Error   | StepTextAnalyzer_StepTextCannotBeNullOrEmptyRule
-RR1002  |  Usage   |  Error   | StepMethodMustReturnVoidOrTaskAnalyzer
-RR1003  |  Usage   |  Error   | AsyncStepMethodMustReturnTaskAnalyzer
+RR1002  |  Usage   | Warning  | StepMethodReturnTypeAnalyzer
+RR1003  |  Usage   |  Error   | AsyncStepMethodReturnTypeAnalyzer
 RR1004  |  Usage   | Warning  | StepTextAnalyzer_StepTextShouldNotHaveLeadingWhitespaceRule
 RR1005  |  Usage   | Warning  | StepTextAnalyzer_StepTextShouldNotHaveTrailingWhitespaceRule
---
+++ Reqnroll.Analyzers/StepDefinitions/StepMethodReturnTypeAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace Reqnroll.Analyzers.StepDefinitions;
+
+using static DiagnosticResources;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class StepMethodReturnTypeAnalyzer : StepMethodAnalyzer
+{
+    #pragma warning disable IDE0090 // Use 'new(...)' - full constructor syntax enables analyzer release tracking.
+    internal static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+        "RR1002",
+        CreateResourceString(nameof(StepMethodShouldReturnVoidOrTaskOrValueTaskTitle)),
+        CreateResourceString(nameof(StepMethodShouldReturnVoidOrTaskOrValueTaskMessage)),
+        DiagnosticCategory.Usage,
+        DiagnosticSeverity.Warning,
+        true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => 
+        ImmutableArray.Create(Rule);
+
+    protected override void AnalyzeStepMethod(SyntaxNodeAnalysisContext context, IMethodSymbol methodSymbol)
+    {
+        var methodSyntax = (MethodDeclarationSyntax)context.Node;
+
+        if (methodSymbol.ReturnsVoid 
+            || methodSymbol.ReturnType.ToDisplayString() == "System.Threading.Tasks.Task"
+            || methodSymbol.ReturnType.ToDisplayString() == "System.Threading.Tasks.ValueTask")
+        {
+            return;
+        }
+
+        var methodName = $"{methodSymbol.ContainingType.Name}.{methodSymbol.Name}";
+
+        context.ReportDiagnostic(
+            Diagnostic.Create(Rule, methodSyntax.Identifier.GetLocation(), methodName, "void"));
+    }
+}
