fa433c47d36464caa21feae871273826cbb4a306
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2026-01-14 14:44:30 -0600

    Fix ReqnrollTagExpression missing ToString() override

--- Reqnroll/Bindings/Discovery/ReqnrollTagExpression.cs
+++ Reqnroll/Bindings/Discovery/ReqnrollTagExpression.cs
@@ -5,14 +5,19 @@ namespace Reqnroll.Bindings.Discovery;
 
 public class ReqnrollTagExpression : ITagExpression
 {
+    public string TagExpressionText { get; }
+    private ITagExpression _inner;
+
     public ReqnrollTagExpression(ITagExpression inner, string tagExpressionText)
     {
         TagExpressionText = tagExpressionText;
         _inner = inner;
     }
-    public string TagExpressionText { get; }
 
-    private ITagExpression _inner;
+    public override string ToString()
+    {
+        return _inner.ToString();
+    }
 
     public virtual bool Evaluate(IEnumerable<string> inputs)
     {
