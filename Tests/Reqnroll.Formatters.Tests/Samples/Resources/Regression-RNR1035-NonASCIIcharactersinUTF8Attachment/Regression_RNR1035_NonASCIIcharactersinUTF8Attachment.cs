022600a3030f181f0507f23b85a683d500fdfc72
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2026-04-14 10:30:38 -0500

    Upgrade Cucumber.HtmlFormatter to v23.1.0 (and Messages to v32.3.1) to finalize the fix for the issue reported in #1035. Added a regression test to Formatters.Tests to demonstrate proper UTF-8 handling of attachments (as a non-CCK test).

---
+++ Tests/Reqnroll.Formatters.Tests/Samples/Resources/Regression-RNR1035-NonASCIIcharactersinUTF8Attachment/Regression_RNR1035_NonASCIIcharactersinUTF8Attachment.cs
@@ -0,0 +1,29 @@
+﻿using Reqnroll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CucumberMessages.CompatibilityTests.CCK.RegressionRNR1035NonASCIIcharactersinUTF8Attachment;
+
+[Binding]
+internal class Attachments
+{
+    private readonly IReqnrollOutputHelper reqnrollOutputHelper;
+
+    internal Attachments(IReqnrollOutputHelper reqnrollOutputHelper)
+    {
+        this.reqnrollOutputHelper = reqnrollOutputHelper;
+    }
+
+    [When(@"attaching the non-ASCII string")]
+    public void WhenAttachTextAs(string text)
+    {
+        // write the string to a file as UTF-8 in current directory
+        var fileName = $"Regression-RNR1035-NonASCIIcharactersinUTF8Attachment.txt";
+        System.IO.File.WriteAllText(fileName, text, Encoding.UTF8);
+        reqnrollOutputHelper.AddAttachment(fileName);
+    }
+
+}
