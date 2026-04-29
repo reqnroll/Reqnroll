7aabb05d453c3a19b826fb8512b1d840b3c5c9d4
Chris Rudolphi <1702962+clrudolphi@users.noreply.github.com>
2026-04-29 08:23:22 -0500

    Update reference to NuGet.CommandLine in Nuget.cs in which a path to the executable is crafted.

--- Tests/TestProjectGenerator/Reqnroll.TestProjectGenerator/NuGet.cs
+++ Tests/TestProjectGenerator/Reqnroll.TestProjectGenerator/NuGet.cs
@@ -57,7 +57,7 @@ namespace Reqnroll.TestProjectGenerator
 
         protected virtual string GetPathToNuGetExe()
         {
-            return Path.Combine(_folders.SystemGlobalNuGetPackages, "NuGet.CommandLine".ToLower(), "6.11.0", "tools", "NuGet.exe");
+            return Path.Combine(_folders.SystemGlobalNuGetPackages, "NuGet.CommandLine".ToLower(), "6.11.2", "tools", "NuGet.exe");
         }
     }
 }
